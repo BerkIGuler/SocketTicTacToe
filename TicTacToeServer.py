f905f175ab9643a8237297d7b02b50b5d7886aaa
BerkIGuler <bierkay1@icloud.com>
2023-05-17 18:13:58 +0300

    Client - client joins the game sends moves if his turn via play function Server - keeps track of turn - assigns id and symbol only once - if no room, player is rejected - updates board after receiving a valid move - notifies client only when 2 players joined - sends your_turn and wait_turn messages to clients utils.py - added move and turn http message templates

--- TicTacToeServer.py
+++ TicTacToeServer.py
@@ -1,11 +1,12 @@
 import logging
+import random
 
-from config import admin_config, update_args
+from config import admin_config
 import socket
 import consts
 
 from logger import get_module_logger
-from parsers import parse_port, HTTPParser
+from parsers import HTTPParser
 from utils import TicTacToeHTTPCommand
 
 import threading
@@ -20,6 +21,8 @@ class GameAdmin:
         self.socket.bind((tcp_config.ip, tcp_config.port))
         self.client_sockets = []
         self.client_addrs = []
+        self.current_turn = random.choice(["X", "O"])
+        self.ttt = TicTacToe()
 
     def start_server(self):
         self.socket.listen()
@@ -28,45 +31,105 @@ class GameAdmin:
         while True:
             client_socket, client_addr = self.socket.accept()
             self.logger.info(f"New connection from {client_addr}")
-            self.client_sockets.append(client_socket)
-            self.client_addrs.append(client_addr)
 
             # Start a new thread to handle the client
             client_thread = threading.Thread(target=self.handle_client, args=(client_socket, client_addr))
             client_thread.start()
 
     def handle_client(self, client_socket, client_addr):
+        assigned = False
+
         try:
-            while True:
+            if len(self.client_sockets) > 1:
+                self.logger.error("No room for a new player!")
+
+            else:
+                # check join request first
                 command = self._receive_command(client_socket)
                 msg = HTTPParser(command).get_json_content()
+                assert msg["type"] == "join", "must receive join request first"
+                p_name = msg["player_name"]
 
-                if msg["type"] == "join":
-                    if len(self.current_players) > 1:
-                        self.logger.error("No room for a new player!")
-                        self.client_sockets.remove(client_socket)
-                        self.client_addrs.remove(client_addr)
-                        break
-                    else:
-                        player_name = msg["player_name"]
-                        self.current_players.append(player_name)
-                        self.respond_symbol_and_id(client_socket)
+                # add the new client to current lists
+                self.client_sockets.append(client_socket)
+                self.client_addrs.append(client_addr)
 
-                    if len(self.current_players) == 2:
-                        self._start_the_game(client_socket)
+                while True:
+                    if len(self.client_sockets) == 2 \
+                            and client_socket in self.client_sockets \
+                            and not assigned:
+                        assigned = True
+                        self._notify_client(client_socket, p_name)
+
+                    if assigned:
+                        self._start_the_game(
+                            client_socket,
+                            client_addr,
+                            p_name)
 
         except Exception as e:
             self.logger.error(f"Error while handling client: {e}")
         finally:
             client_socket.close()
 
-    def respond_symbol_and_id(self, client_socket):
-        new_p_id = len(self.current_players) - 1
-        symbol = ["X", "O"][new_p_id]
-        http_resp = TicTacToeHTTPCommand().symbol_and_id(symbol, new_p_id)
-        self.logger.info(f"Assigned ID {new_p_id} and symbol {symbol} to "
-                         f"{self.current_players[new_p_id]}")
+    def _start_the_game(self, client_socket, client_addr, name):
+        self._send_turn(client_socket, client_addr, name)
+        msg = self._receive_command(client_socket)
+        msg = HTTPParser(msg).get_json_content()
+        if self._valid_move(client_socket, msg):
+            self._update_board(msg, client_socket)
+        else:
+            # self._send_invalid_move_msg()
+            pass
+
+    def _notify_client(self, client_socket, p_name):
+        p_id, symbol = self._get_ip_and_sym(client_socket)
+        http_resp = TicTacToeHTTPCommand().symbol_and_id(symbol, p_id)
+        self.logger.info(f"Assigned ID {p_id} and symbol {symbol} to "
+                         f"{p_name}")
         client_socket.sendall(http_resp)
+
+    def _send_turn(self, client_socket, client_addr, name):
+        client_ip = client_addr[0]
+        p_id, symbol = self._get_ip_and_sym(client_socket)
+
+        if symbol == self.current_turn:
+            msg = TicTacToeHTTPCommand().turn(
+                client_ip, p_id, symbol,
+                board_state=TicTacToe.encode(self.ttt.board),
+                your_turn=True
+            )
+            client_socket.sendall(msg)
+        else:
+            msg = TicTacToeHTTPCommand().turn(
+                client_ip, p_id, symbol,
+                board_state=TicTacToe.encode(self.ttt.board),
+                your_turn=False
+            )
+            client_socket.sendall(msg)
+
+        # self.current_turn = "X" if self.current_turn == "O" else "O"
+        self.logger.info(f"Sent board info to {name}")
+
+    def _valid_move(self, client_socket, msg):
+        valid = False
+        p_id, symbol = self._get_ip_and_sym(client_socket)
+        if msg["type"] == "move" and int(msg["id"]) == p_id:
+            row, col = int(msg["row"]), int(msg["col"])
+            if self.ttt.validate_move((row, col)):
+                valid = True
+        return valid
+
+    def _get_ip_and_sym(self, client_socket):
+        p_id = self.client_sockets.index(client_socket)
+        symbol = ["X", "O"][p_id]
+        return p_id, symbol
+
+    def _update_board(self, msg, client_socket):
+        _, sym = self._get_ip_and_sym(client_socket)
+        row, col = int(msg["row"]), int(msg["col"])
+        self.ttt.update_board(sym, (row, col))
+        print(self.ttt)
 
     @staticmethod
     def _receive_command(client_socket):
@@ -97,13 +160,28 @@ class TicTacToe:
     Xs = ["X", "X", "X"]
     Os = ["O", "O", "O"]
 
-    def __init__(self):
-        self.board = [["-", "-", "-"],
-                      ["-", "-", "-"],
-                      ["-", "-", "-"]]
+    def __init__(self, board=None):
+        if board:
+            self.board = board
+        else:
+            self.board = [["-", "-", "-"],
+                          ["-", "-", "-"],
+                          ["-", "-", "-"]]
+
+    @classmethod
+    def encode(cls, board):
+        flattened = sum(board, [])
+        encoded_board = ".".join(flattened)
+        return encoded_board
+
+    @classmethod
+    def decode(cls, board):
+        chars = board.split(".")
+        decoded_board = [chars[0:3], chars[3:6], chars[6:]]
+        return cls(decoded_board)
 
     def update_board(self, sym, pos):
-        if self._validate_move(pos) and sym.upper() in ["X", "O"]:
+        if self.validate_move(pos) and sym.upper() in ["X", "O"]:
             row, col = pos
             self.board[row][col] = sym.upper()
             status = consts.VALID_MOVE
@@ -140,13 +218,10 @@ class TicTacToe:
 
         return winner
 
-    def _validate_move(self, move):
+    def validate_move(self, move):
         valid = True
         row, col = move
-        if move not in self.valid_moves:
-            valid = False
-
-        if self.board[row][col] != "-":
+        if move not in self.valid_moves or self.board[row][col] != "-":
             valid = False
 
         return valid
@@ -174,4 +249,3 @@ if __name__ == "__main__":
 
     admin = GameAdmin(tcp_config=admin_config, logger=logger)
     admin.start_server()
-    admin.new_game()
